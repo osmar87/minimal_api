b7954a49054c94dc864426d507495e0eb102bc07
osmar gomes <osmar.dev87@gmail.com>
2025-10-04 12:06:41 -0300

    api minimal concluida

---
+++ src/daddo.ts
@@ -0,0 +1,39 @@
+const teams = [
+  { id: 1, name: "Red Bull Racing", base: "Reino Unido" },
+  { id: 2, name: "Racing Bulls (RB)", base: "Itália" },
+  { id: 3, name: "Scuderia Ferrari", base: "Itália" },
+  { id: 4, name: "Mercedes-AMG Petronas", base: "Reino Unido" },
+  { id: 5, name: "McLaren", base: "Reino Unido" },
+  { id: 6, name: "Williams", base: "Reino Unido" },
+  { id: 7, name: "Aston Martin Aramco F1 Team", base: "Reino Unido" },
+  { id: 8, name: "Haas F1 Team", base: "Estados Unidos" },
+  { id: 9, name: "BWT Alpine F1 Team", base: "Reino Unido" },
+  { id: 10, name: "Kick Sauber (Stake/Kick Sauber)", base: "Suíça" }
+];
+
+const drivers = [
+  { id: 1, name: "Max Verstappen", teamId: 1 },
+  { id: 2, name: "Sergio Pérez", teamId: 1 },
+  { id: 3, name: "Yuki Tsunoda", teamId: 2 },
+  { id: 4, name: "Liam Lawson", teamId: 2 },
+  { id: 5, name: "Charles Leclerc", teamId: 3 },
+  { id: 6, name: "Lewis Hamilton", teamId: 3 },
+  { id: 7, name: "George Russell", teamId: 4 },
+  { id: 8, name: "Andrea Kimi Antonelli", teamId: 4 },
+  { id: 9, name: "Lando Norris", teamId: 5 },
+  { id: 10, name: "Oscar Piastri", teamId: 5 },
+  { id: 11, name: "Alex Albon", teamId: 6 },
+  { id: 12, name: "Logan Sargeant", teamId: 6 },
+  { id: 13, name: "Fernando Alonso", teamId: 7 },
+  { id: 14, name: "Lance Stroll", teamId: 7 },
+  { id: 15, name: "Nico Hülkenberg", teamId: 8 },
+  { id: 16, name: "Esteban Ocon", teamId: 8 },
+  { id: 17, name: "Pierre Gasly", teamId: 9 },
+  { id: 18, name: "Jack Doohan", teamId: 9 },
+  { id: 19, name: "Valtteri Bottas", teamId: 10 },
+  { id: 20, name: "Zhou Guanyu", teamId: 10 }
+];
+
+
+
+export { drivers, teams}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,59 @@
-console.log("hello world");
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { drivers, teams } from "./daddo";
+
+interface DriversParam {
+    id: string
+}
+
+
+const server = fastify({ logger: true})
+
+// Habilita CORS
+server.register(cors, {
+  origin: "*", // permite qualquer origem
+});
+
+
+
+server.get("/teams", async (request, response) => {
+    response.type("application/json").code(200);
+    return {teams};
+})
+
+server.get("/drivers", async (request, response) => {
+    response.type("application/json").code(200);
+    return {drivers};
+})
+
+server.get<{Params: DriversParam}>("/drivers/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    const driver = drivers.find(d => d.id === id)
+
+    if (!driver) {
+        response.type("application/json").code(404);
+        return {msg: "Driver not found"}
+    }
+
+    const team = teams.find(d => d.id === driver.teamId)
+
+    if (!team) {
+        response.type("application/json").code(404);
+        return {msg: "Team not found"}
+    }
+    response.type("application/json").code(200);
+    return { driver : {
+        id: driver.id,
+        name: driver.name,
+        team: team.name
+    }}
+
+})
+
+
+server.listen({
+    port: 3333,
+    host: "0.0.0.0"
+}, () =>{
+    console.log("Server init");
+})
